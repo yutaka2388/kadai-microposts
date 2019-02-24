5c6da0c025013a4da2e73ed5fb74fb0014ef2c96
EC2 Default User <ec2-user@ip-172-31-77-53.ec2.internal>
2019-02-24 11:35:45 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
-
   def new
     @user = User.new
   end
